1e6914ce809ee43934d864447508c01c63fe3160
Aleksey Shipulya <chaosal@yandex.ru>
2015-12-05 18:29:16 +0500

    add Sphinx assets folder autoupdate

--- app/src/main/java/com/github/chaosal/androidroshead/SpeechRecognitionNode.java
+++ app/src/main/java/com/github/chaosal/androidroshead/SpeechRecognitionNode.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.media.AudioManager;
 import android.media.ToneGenerator;
 import android.util.Log;
-import android.widget.Toast;
 
 import org.ros.namespace.GraphName;
 import org.ros.node.AbstractNodeMain;
@@ -13,8 +12,8 @@ import org.ros.node.Node;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.logging.Logger;
 
+import edu.cmu.pocketsphinx.Assets;
 import edu.cmu.pocketsphinx.Hypothesis;
 import edu.cmu.pocketsphinx.RecognitionListener;
 import edu.cmu.pocketsphinx.SpeechRecognizer;
@@ -24,7 +23,6 @@ import edu.cmu.pocketsphinx.SpeechRecognizerSetup;
 public class SpeechRecognitionNode extends AbstractNodeMain implements RecognitionListener {
     private final static String CONTEXT = SpeechRecognitionNode.class.getSimpleName();
     public final static String TOPIC = "/speechRecognition";
-    private final static String ASSETS_DIR = "ros/sphinx";
     private final static String KWS_SEARCH_NAME = "selfName";
     private final static String GRAMMAR_SEARCH = "grammar";
     private final Context context;
@@ -45,15 +43,16 @@ public class SpeechRecognitionNode extends AbstractNodeMain implements Recogniti
     public void onStart(ConnectedNode connectedNode) {
         super.onStart(connectedNode);
 
-        final File assetsDir = new File(context.getExternalFilesDir(null), ASSETS_DIR);
-
         try {
+            Assets assets = new Assets(context);
+            File assetsDir = assets.syncAssets();
+
             recognizer = SpeechRecognizerSetup.defaultSetup().setAcousticModel(new File(assetsDir, "acc-model"))
                     .setDictionary(new File(assetsDir, "cmudict.dict"))
                             // To disable logging of raw audio comment out this call (takes a lot of space on the device)
-                    .setRawLogDir(assetsDir)
+                            //.setRawLogDir(assetsDir)
                             // Threshold to tune for keyphrase to balance between false alarms and misses
-                    .setKeywordThreshold(1e-20f)
+                    .setKeywordThreshold(1e-30f)
                             // Use context-independent phonetic search, context-dependent is too slow for mobile
                     .setBoolean("-allphone_ci", true)
                     .getRecognizer();
@@ -68,16 +67,14 @@ public class SpeechRecognitionNode extends AbstractNodeMain implements Recogniti
         }
     }
 
-    private void startSearch()
-    {
+    private void startSearch() {
         Log.i(CONTEXT, "start search");
         recognizer.stop();
         recognizer.startListening(GRAMMAR_SEARCH, 10000);
         toneGenerator.startTone(ToneGenerator.TONE_PROP_BEEP);
     }
 
-    private void startListen()
-    {
+    private void startListen() {
         recognizer.stop();
         recognizer.startListening(KWS_SEARCH_NAME);
     }
@@ -94,20 +91,19 @@ public class SpeechRecognitionNode extends AbstractNodeMain implements Recogniti
 
     @Override
     public void onPartialResult(Hypothesis hypothesis) {
-        if(hypothesis == null)
+        if (hypothesis == null)
             return;
 
-        if(hypothesis.getHypstr().equals(context.getString(R.string.recognitionName)))
+        if (hypothesis.getHypstr().equals(context.getString(R.string.recognitionName)))
             startSearch();
     }
 
     @Override
     public void onResult(Hypothesis hypothesis) {
-        if(hypothesis == null || Math.abs(hypothesis.getBestScore()) > 2000)
+        if (hypothesis == null || Math.abs(hypothesis.getBestScore()) > 2000)
             return;
-        final String text = hypothesis != null ? hypothesis.getHypstr() : "null result";
         Log.i(CONTEXT, String.format("score: %d, probe: %d, result: %s",
-                hypothesis.getBestScore(), hypothesis.getProb(), text));
+                hypothesis.getBestScore(), hypothesis.getProb(), hypothesis.getHypstr()));
     }
 
     @Override
